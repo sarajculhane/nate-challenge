43bcf17f382a2ee93fcd50c2c26cc40b87d4da87
Sara Culhane <sjcully@mac.com>
2021-01-11 13:27:18 -0500

    click start on page 1

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const page = async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://nate-eu-west-1-prediction-test-webpages.s3-eu-west-1.amazonaws.com/tech-challenge/page1.html');
+    await page.click('[type="button" i]')
 
 }
 
